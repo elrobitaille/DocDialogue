739c1760e6fffaa9e703990387e3c2c3adf95f43
Rapuris <91913098+Rapuris@users.noreply.github.com>
2023-09-16 21:34:27 -0400

    changed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const OPENAI_URL = 'https://api.openai.com/v1/chat/completions';
 // Input Your OpenAI API Key Here. 
 // You can sign up and get API Key from here 
 // https://platform.openai.com/account/api-keys
-let apiKey = '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';
+let apiKey = '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';
 const messages = []; // store previous messages to remember whole conversation
 
 // Function to add a chat message to the container
